34f0c479000a8000b5d6b8e66fca99b8bef87203
BARKER, Kyle (Student) <kb477@students.waikato.ac.nz>
2025-10-10 19:34:05 +0200

    refactored program to fit docker distributed architecture

--- br.sh
+++ br.sh
@@ -6,6 +6,10 @@ set -e  # Exit immediately if a command exits with a non-zero status
 
 echo "=== Cleaning and packaging project with Maven ==="
 ./mvnw clean package -DskipTests -e
+echo "=== Recreating Docker network ==="
+docker network rm ev_network 2>/dev/null || true
+docker network create ev_network
+cd central
 
 echo "=== Stopping running Docker containers ==="
 docker-compose down
